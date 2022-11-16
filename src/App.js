89ed752620b5220675b830326a8fbf0de748bc68
Sagni-Prog <sagnialemayehu69@gmail.com>
2022-11-16 15:57:04 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import {FaArrowAltCircleLeft,FaArrowAltCircleRight} from 'react-icons/fa'
 import { useEffect, useState } from 'react';
 import Slide from './Slide';
 import { Quote } from './Quote';
+import Image from './Image';
 
 function App() {
   return(
     <div>
-       {/* <Slide /> */}
-       <Quote />
+       <Slide />
+       {/* <Quote /> */}
     </div>
   );
 
---
+++ src/Image.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import image from './asset/images/me1.jpg'
+
+const Image = () => {
+  return (
+    <div>
+        <img src = {image} alt = 'hello'/>
+    </div>
+  )
+}
+
+export default Image
